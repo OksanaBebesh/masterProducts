2de70c97ade43667f5ab371fe3d4dfe38592b05b
OksanaBebesh <oksana.bebesh1984@gmail.com>
2024-05-15 16:39:27 +0200

    feat: add Products css style

--- vue-project/src/main.jsx
+++ vue-project/src/main.jsx
@@ -3,12 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import Vuex from 'vuex'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(Vuex)
-app.use(store)
 app.mount('#app')
--- vue-project/src/views/ProductsView.vue
+++ vue-project/src/views/ProductsView.vue
@@ -10,25 +10,27 @@ import axios from "axios";
       <DocumentationIcon />
     </template>
     <template #heading>Products</template>
-    <button @click="getProducts">Get Produtcs</button>
-    <button @click="getMoreProducts" :disabled="countProductsOnPage == 20">Get More Produtcs</button>
+    <div class="btn-line">
+      <button type="button" class="btn btn-info" @click="getProducts">Get Produtcs</button>
+      <button type="button" class="btn btn-primary" @click="getMoreProducts" :disabled="disableButton">Get More Produtcs</button>
+    </div>
     <div class="table-products">
-      <div class="row-header">
-        <div>N</div>
+      <div class="row-header table">
+        <div class="id-column">#</div>
         <div>Product title</div>
         <div>Price</div>
         <div>Description</div>
         <div>Photo</div>
       </div>
       <div class="row-product" v-for="product in arrayProducts">
-        <div>{{ product.id }}</div>
+        <div class="id-column">{{ product.id }}</div>
         <div>{{ product.title }}</div>
         <div>{{ product.price }}</div>
-        <div>{{ product.description }}</div>
+        <div>{{ product.description.substring(0,50) }}</div>
         <div><img class="product-photo" :src="`${product.image}`" /></div>
       </div>
     </div>
-    <button @click="getMoreProducts" :disabled="countProductsOnPage == 20">Get More Produtcs</button>
+    <button type="button" class="btn btn-primary" @click="getMoreProducts" :disabled="countProductsOnPage == 20">Get More Produtcs</button>
   </DesciptionItem>
 </template>
 
@@ -44,18 +46,24 @@ export default {
       allPtoducts: [],
       arrayAllProducts: [],
       isProductsLoading: false,
+      maxCountProducts: 0,
     };
   },
   methods: {
+    disableButton() {
+      let isDisabled;
+      return isDisabled;
+    },
+    
     getProducts() {
       this.fetchProductsData();
     },
+
     getMoreProducts() {
       this.countProductsOnPage += 5;
       this.fetchProductsData();
     },
     
-
     async fetchProductsData() {
       try {
         this.isProductsLoading = true;
@@ -66,17 +74,7 @@ export default {
       } finally {
         this.isProductsLoading = false;
       }
-
     },
-
-    fetchAllProductsData() {
-      fetch(
-        "https://fakestoreapi.com/products"
-      )
-        .then((response) => response.json())
-        .then((json) => (this.arrayAllProducts = json));
-    },
-
   },
 };
 </script>
@@ -88,11 +86,24 @@ export default {
   justify-content: space-between;
   width: 43rem;
 }
+
 .row-header,
 .row-product {
   display: flex;
   justify-content: space-between;
   width: 100%;
+}
+
+.row-product {
+  padding: 15px;
+  border: 1px solid rgb(174, 169, 169);
+  border-radius: 5px;
+  margin-bottom: 2px;
+}
+
+.row-product:hover {
+  cursor: pointer;
+  border: 2px solid rgb(61, 134, 75);
 }
 
 .row-product div {
@@ -101,6 +112,27 @@ export default {
 }
 
 .product-photo {
-  width: 150px;
+  width: 100px;
+  box-shadow: 10px 5px 5px rgb(174, 169, 169);
+  border-radius: 5px;
+}
+
+.btn-line {
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.row-header {
+  border-bottom: 1px solid black;
+}
+
+.row-header div {
+  font-weight: 500;
+}
+
+.id-column {
+  width: 25px;
 }
 </style>
