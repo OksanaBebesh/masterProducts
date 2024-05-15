f7a78546a2d499625cc67130e81d03245f5d547f
OksanaBebesh <oksana.bebesh1984@gmail.com>
2024-05-15 17:04:54 +0200

    refactor: remove unuseful

--- vue-project/src/components/CustomersAddForm.vue
+++ vue-project/src/components/CustomersAddForm.vue
@@ -5,8 +5,8 @@
 <template>
     <div class="form customer-add">
         <h2>Add new Customer</h2>
-        <input type="text" v-model="customerName" placeholder="Customer Name"  />
-        <input type="email" v-model="customerEmail" placeholder="Customer Email" />
+        <input type="text" v-model="customer.name" placeholder="Customer Name"  />
+        <input type="email" v-model="customer.email" placeholder="Customer Email" />
         <div class="btn-lines">
             <button class="btn btn-success" @click="addCustomers">Save</button>
             <button class="btn btn-danger" @click="replaceToMainCustomerPage">Cancel</button>
@@ -19,8 +19,6 @@ export default {
     data(){
         return {
             customer: {name: "", email: ""},
-            customerName: "",
-            customerEmail: "",
             customersPOSTUrl: "http://localhost:8000/api/customers"
         }
     },
@@ -30,9 +28,6 @@ export default {
         },        
         async addCustomers() {
             try {
-                this.customer.name = this.customerName;
-                this.customer.email = this.customerEmail;
-
                 await axios.post(this.customersPOSTUrl, this.customer);
             } 
             catch(e) 
